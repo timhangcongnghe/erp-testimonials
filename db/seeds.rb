df14e13e2a677f318b45cb4f42c609d778cf0b83
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-16 18:27:24 -0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 puts "Create sample testimonials"
-Erp::Testimonials::Testimonial.where(author: "Anh Luân").destroy_all
-testimonial1 = Erp::Testimonials::Testimonial.create(author: "Anh Luân", content: "Nội dung đang được cập nhật...")
-Erp::Testimonials::Testimonial.where(author: "Anh Sơn").destroy_all
-testimonial2 = Erp::Testimonials::Testimonial.create(author: "Anh Sơn", content: "Nội dung đang được cập nhật...")
-Erp::Testimonials::Testimonial.where(author: "Anh Hùng Đẹp Trai").destroy_all
-testimonial3 = Erp::Testimonials::Testimonial.create(author: "Anh Hùng", content: "Nội dung đang được cập nhật...")
+Erp::Testimonials::Testimonial.where(author: "Mr. Brian").destroy_all
+testimonial1 = Erp::Testimonials::Testimonial.create(author: "Mr. Brian", content: "Nội dung đang được cập nhật...")
+Erp::Testimonials::Testimonial.where(author: "Mr. Alfred").destroy_all
+testimonial2 = Erp::Testimonials::Testimonial.create(author: "Mr. Alfred", content: "Nội dung đang được cập nhật...")
+Erp::Testimonials::Testimonial.where(author: "Mrs. Pearl").destroy_all
+testimonial3 = Erp::Testimonials::Testimonial.create(author: "Mrs. Pearl", content: "Nội dung đang được cập nhật...")
